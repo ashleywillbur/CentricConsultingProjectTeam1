86ca99e5f37c68858e0b394385b696ad067ea423
Willbur <aw344317@ohio.edu>
2021-10-29 10:18:23 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CentricConsultingProject.Startup))]
+namespace CentricConsultingProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
